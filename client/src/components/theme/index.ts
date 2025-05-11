bbb71502bc545c765bc44bed8a48f8a8ba53f49a
Dave Williams <dave@dave.io>
2025-05-12 00:36:07 +0100

    🐛 fix(clear): clear out to be replaced by Rails implementation from spec

--- client/src/components/theme/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './catppuccin-theme-provider'
-export * from './theme-selector'
-export * from './theme-utils'
--- run-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Script to run Vitest tests
-
-echo "Running Vitest tests..."
-npx vitest run
